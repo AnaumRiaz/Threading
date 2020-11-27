f8dccbaafae09ad302185d030e3e7038193c0988
AnaumRiaz <anaum.riaz91@gmail.com>
2020-11-26 17:18:54 -0800

    first multuthreading program. makes two threads. reads values from an array. adds them and gives the result

---
+++ mt_vect.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <thread>
+#include <vector>
+#include<numeric>
+
+void sum_nums(int array[], int &sum, int start, int end) {
+  sum=0;
+  for (int i=start; i<end; i++) {
+    sum+= array[i];
+  }
+}
+
+int main()
+{
+  int sum=0;
+  const int size_array = 10000;
+  const int num_threads = 2; 
+  std::vector<std::thread> my_threads;
+  std::vector<int> partial_sums(num_threads);
+  int array[size_array];
+  std::fill(array, array + size_array, 1); 
+  int step = size_array / num_threads;
+
+  
+  for (int i = 0; i <num_threads; i++) {
+    my_threads.push_back(std::thread(sum_nums, std::ref(array), std::ref(partial_sums[i]), i*step, (i+1)*step));
+  }
+
+  
+  sum_nums(array, sum, 0, size_array);
+  std::cout<< "The sum(nonthreaded) is "<< sum << std::endl;
+  
+
+   for (std::thread &t: my_threads) {
+    if (t.joinable()) {
+      t.join();
+    }
+  }
+ std::cout << "Sum using threads = " << std::accumulate(partial_sums.begin(), partial_sums.end(), 0) << std::endl; 
+
+  return 0;
+
+}
